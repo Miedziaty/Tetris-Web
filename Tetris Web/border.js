a11a37a6854cad2fc8b719ec350d44bff11c0ef1
Miedziaty <trelaolivier1@gmail.com>
2024-10-07 13:40:30 +0200

    Git

--- Tetris Web/border.js
+++ Tetris Web/border.js
@@ -5,6 +5,6 @@
 export function drawBorder() {
     context.strokeStyle = '#000';  // Set the border color
     context.lineWidth = 5;         // Set the border thickness
-    context.strokeRect(0, 0, canvas.width, canvas.height); // Draw the border around the canvas
+    context.strokeRect(0, 0, canvas.height, canvas.width); // Draw the border around the canvas
 }
 
